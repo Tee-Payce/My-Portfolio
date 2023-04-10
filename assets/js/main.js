6a1afbeeebb5862507ee3fcda3a20fbfff50c6fa
Tee-Payce <114886406+Tee-Payce@users.noreply.github.com>
2023-04-10 11:47:01 +0200

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,32 @@
+
+(function($) {
+
+	var	$window = $(window),
+		$body = $('body');
+
+	// Breakpoints.
+		breakpoints({
+			wide:      [ '1281px',  '1680px' ],
+			normal:    [ '981px',   '1280px' ],
+			narrow:    [ '841px',   '980px'  ],
+			narrower:  [ '737px',   '840px'  ],
+			mobile:    [ '481px',   '736px'  ],
+			mobilep:   [ null,      '480px'  ]
+		});
+
+	// Play initial animations on page load.
+		$window.on('load', function() {
+			window.setTimeout(function() {
+				$body.removeClass('is-preload');
+			}, 100);
+		});
+
+})(jQuery);
+
+
+//typed
+var typed = new Typed('#typed',{
+	strings: ["I am a Software Engineering student at Harare Institute of Technology.<br>Welcome to my professional portifolio Traverse with me as I face my journey in the world of code </br>"],
+	typeSpeed: 15,
+	loop: false
+});
